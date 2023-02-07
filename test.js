6917209fb25df5f686b7d2540ec02cfc505bbc14
Anton Dutchak <dydahmel@gmail.com>
2023-02-07 11:38:51 -0500

    s 6 complete

--- test.js
+++ test.js
@@ -63,3 +63,126 @@ form.addEventListener("input", function (evt) {
 form.addEventListener("submit", function (evt) {
   // processing of the submit event
 }); 
+
+// change input behavior based on validation
+
+// First, select all the needed form elements, and assign them to variables
+const formElement = document.querySelector(".form");
+const formInput = formElement.querySelector(".form__input");
+
+// Code the 1st function, which shows the error element
+const showInputError = (element) => {
+  element.classList.add("form__input_type_error");
+};
+
+// Code the 2nd function, which hides the error element
+const hideInputError = (element) => {
+  element.classList.remove("form__input_type_error");
+};
+
+// Code the 3rd function, which checks if the field is valid
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    // If NOT (!), show the error element
+    showInputError(formInput);
+  } else {
+    // If it's valid, hide the error element
+    hideInputError(formInput);
+  }
+};
+ 
+formElement.addEventListener("submit", function (evt) {
+  // Cancel the browser default action, so that clicking on the submit button won't refresh the page
+  evt.preventDefault();
+});
+
+// Call the isValid() function for each character input
+formInput.addEventListener("input", isValid);
+
+
+//template litearl as const!!!!!!!!!
+const formError = formElement.querySelector(`.${formInput.id}-error`); 
+
+
+
+/* adding message as span element*/
+const formElement = document.querySelector(".form");
+const formInput = formElement.querySelector(".form__input");
+
+// Select each error message element using its unique class name
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showInputError = (element) => {
+  element.classList.add("form__input_type_error");
+  // Show the error message
+  formError.classList.add("form__input-error_active");
+};
+
+const hideInputError = (element) => {
+  element.classList.remove("form__input_type_error");
+  // Hide the error message
+  formError.classList.remove("form__input-error_active");
+};
+
+
+
+//show error for multiple inputs
+const showInputError = (formElement, inputElement, errorMessage) => {
+  // Find the error message element inside the very function
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // The rest remains unchanged
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  // Find the error message element
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // The rest remains unchanged
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+}; 
+
+
+
+// set listeners for multiple inputs
+const setEventListeners = (formElement) => {
+  // Find all fields inside the form, and
+  // make an array from them using the Array.from() method
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+
+  // Iterate over the resulting array
+  inputList.forEach((inputElement) => {
+    // add the input event handler to each field
+    inputElement.addEventListener("input", () => {
+      // Call the isValid() function inside the callback,
+      // and pass the form and the element to be checked to it
+      isValid(formElement, inputElement)
+    });
+  });
+};
+
+
+// set event handlers for multiple forms
+const enableValidation = () => {
+  // It will find all forms with the specified class in DOM, and
+  // make an array from them using the Array.from() method
+  const formList = Array.from(document.querySelectorAll(".form"));
+
+  // Iterate over the resulting array
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      // Cancel default behavior for each form
+      evt.preventDefault();
+    });
+
+    // Call the setEventListeners() function for each form,
+    // taking a form element as an argument
+    setEventListeners(formElement);
+  });
+};
+
+// Call the function
+enableValidation();
